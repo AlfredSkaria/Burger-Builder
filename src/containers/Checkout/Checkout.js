18f475850e08f1dc3abf048292e601111e820473
Alfred SKaria <alfiskaria@gmail.com>
2020-06-11 23:38:09 +0530

    added contact data page for the submitting the data about the customer

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import {Route} from 'react-router-dom';
+import ContactData from './ContactData/ContactData';
 
 
 class Checkout extends Component{
@@ -35,9 +37,12 @@ class Checkout extends Component{
     render(){
         return(
             <div>
-                <CheckoutSummary ingredients={this.state.ingredients}
+                <CheckoutSummary 
+                ingredients={this.state.ingredients}
                 checkoutCancelled={this.checkoutCancelledHandler}
                 checkoutContinued={this.checkoutContinuedHandler}/>
+
+                <Route path={this.props.match.path + '/contact-data'} component={ContactData}/>
             </div>
         );
     }
