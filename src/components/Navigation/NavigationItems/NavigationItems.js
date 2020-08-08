d1202f78c924e6cb73683b2b0adfe51e74eced2b
Alfred SKaria <alfiskaria@gmail.com>
2020-08-08 20:16:43 +0530

    Added logout route to the project.

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -2,11 +2,15 @@ import React from 'react';
 import classes from './NavigationItems.css';
 import NavigationItem from './NavigationItem/NavigationItem';
 
-const navigationItems = () => (
+const navigationItems = (props) => (
     <ul className={classes.NavigationItems}>
         <NavigationItem link = "/" exact>Burger Builder</NavigationItem>
         <NavigationItem link = "/orders">orders</NavigationItem>
-        <NavigationItem link = "/auth">Authentication</NavigationItem>
+        {
+            !props.isAuthenticated ?  
+            <NavigationItem link = "/auth">Authentication</NavigationItem>:
+            <NavigationItem link = "/logout">Logout</NavigationItem>
+        }
     </ul>
 );
 
